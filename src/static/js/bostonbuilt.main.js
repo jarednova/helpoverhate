97a3a0be35506824d0baf6c5f8c948c00842ef0d
Holly Copeland <hollycopeland@Hollys-MacBook-Air.local>
2016-11-11 11:39:02 -0500

    fixed contact info display, removed category truncation toggle

--- src/static/js/bostonbuilt.main.js
+++ src/static/js/bostonbuilt.main.js
@@ -39,54 +39,54 @@
     $('.contact-info').children('a').each(truncateURL);
   });
 
-  $(document).ready(collapseCategories);
-  $(window).resize(collapseCategories);
-  $(document).ready(showCategories);
-  $(window).resize(showCategories);
+  // $(document).ready(collapseCategories);
+  // $(window).resize(collapseCategories);
+  // $(document).ready(showCategories);
+  // $(window).resize(showCategories);
 
 
-  function collapseCategories() {
-    if ($(window).width() <= 768){  
-      // do something here
-      $('.categories__list').children('li').each(function (i)
-        {
-          if (i > 3) {
-            $(this).hide();   
-          }
-        });
+  // function collapseCategories() {
+  //   if ($(window).width() <= 768){  
+  //     // do something here
+  //     $('.categories__list').children('li').each(function (i)
+  //       {
+  //         if (i > 3) {
+  //           $(this).hide();   
+  //         }
+  //       });
 
-      var categories__more = $('<li class="categories__list--category more"><a href="">More...</a></li>');
+  //     var categories__more = $('<li class="categories__list--category more"><a href="">More...</a></li>');
       
-      $('.categories__list').append(categories__more);
+  //     $('.categories__list').append(categories__more);
 
-      categories__more.find('a').on('click', function (){
-        if ($(this).hasClass('expanded')) {
-          $(this).removeClass('expanded');
-          $(this).text("More...");
-          $(this).parent().siblings('li').each(function (i) {
-            if (i > 5) {
-              $(this).fadeOut();   
-            }
-          });
-        } else {
-          $(this).addClass('expanded'); 
-          $(this).parent().siblings('li').fadeIn();
-          $(this).text('Less...');
-        }
+  //     categories__more.find('a').on('click', function (){
+  //       if ($(this).hasClass('expanded')) {
+  //         $(this).removeClass('expanded');
+  //         $(this).text("More...");
+  //         $(this).parent().siblings('li').each(function (i) {
+  //           if (i > 5) {
+  //             $(this).fadeOut();   
+  //           }
+  //         });
+  //       } else {
+  //         $(this).addClass('expanded'); 
+  //         $(this).parent().siblings('li').fadeIn();
+  //         $(this).text('Less...');
+  //       }
 
-        return false; 
-      });
-    }
-  }
+  //       return false; 
+  //     });
+  //   }
+  // }
 
-  function showCategories() {
-    if ($(window).width() > 768) {
-      $('.categories__list').children('.more').each(function() {
-        $(this).remove();
-      });
-      $('.categories__list').children('li').each(function (i) {
-        $(this).show();   
-      });
-    }
-  }
+  // function showCategories() {
+  //   if ($(window).width() > 768) {
+  //     $('.categories__list').children('.more').each(function() {
+  //       $(this).remove();
+  //     });
+  //     $('.categories__list').children('li').each(function (i) {
+  //       $(this).show();   
+  //     });
+  //   }
+  // }
 })(jQuery);
